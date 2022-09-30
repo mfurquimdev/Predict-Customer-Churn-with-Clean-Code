2220ca8591291a60bb6ec5596c6e11bf85ccf957
Mateus Furquim <furquim.axs@gmail.com>
2022-09-29 23:55:59 -0300

    Organize unit tests by classes

--- tests/test_churn_library.py
+++ tests/test_churn_library.py
@@ -5,11 +5,26 @@ from churn_library import import_data
 from churn_library import logger
 
 
-@patch.dict(os.environ, {"PATH_TO_DATA_FOLDER": "tests/data"})
-def test_import_data():
-    csv_name = "fake.csv"
+class TestImportData:
+    @patch.dict(os.environ, {"PATH_TO_DATA_FOLDER": "tests/data"})
+    def test_fake_data(self):
+        csv_name = "fake.csv"
 
-    df = import_data(csv_name)
+        df = import_data(csv_name)
 
-    assert df.shape[0] > 0
-    assert df.shape[1] > 0
+        assert df.shape[0] > 0
+        assert df.shape[1] > 0
+
+    @patch.dict(os.environ, {"PATH_TO_DATA_FOLDER": "data"})
+    def test_real_data(self):
+        csv_name = "BankChurners.csv"
+
+        df = import_data(csv_name)
+
+        assert df.shape == (10127, 23)
+
+
+class TestPerformEDA:
+    @patch.dict(os.environ, {"PATH_TO_IMAGE_FOLDER": "tests/image"})
+    def test_perform_eda(self):
+        pass
